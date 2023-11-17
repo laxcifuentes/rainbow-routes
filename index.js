a06756525e5c7ca55e10e92a0aee852b8b5281b5
yournewgoodname <yournew@email.com>
2023-11-16 21:39:03 -0800

    added .env file

---
+++ index.js
@@ -0,0 +1,20 @@
+// getting environment variables
+require ('dotenv').config()
+
+// requiring needed node modules
+const express = require('express')
+const app = express()
+
+// Declare routes that people can visit on the application.
+app.get('/', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `)
+})
+
+app.listen(process.env.PORT)

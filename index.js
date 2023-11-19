f6c39ef7b217bcf08714862af49dd3f5b06386de
yournewgoodname <yournew@email.com>
2023-11-18 21:11:16 -0800

    updated index.js

--- index.js
+++ index.js
@@ -17,4 +17,33 @@ app.get('/', function (req, res) {
     `)
 })
 
+// Teal Page
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
+        </body>
+    `)
+})
+
+// Crimson Page
+// app.get('/crimson', function (req, res) {
+//     res.send(`
+//         <body style="margin: 0;">
+//             <div style="border: 1px solid black; height: 10vh; background-color: crimson;">
+//                 <h2 style="text-align: center;">NAV BAR</h2>
+//             </div>
+//             <h1 style="color: crimson;">Crimson Page</h1>
+//         </body>
+//     `)
+// })
+
+app.post('/:color', (req, res) => {
+    res.send('Hello world')
+})
+
 app.listen(process.env.PORT)
